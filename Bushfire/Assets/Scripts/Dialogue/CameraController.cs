7ceaf7ae4e6fc76c154626034f75395d0b0879d5
Thomas Ingram <thomasingram@vertx.xyz>
2018-02-23 15:02:05 +1100

    Added basic camera
    Camera

---
+++ Bushfire/Assets/Scripts/Dialogue/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Vector2 cameraSpeed = Vector2.one;
+	private Transform t;
+
+	private bool isLocked;
+
+	private void OnEnable()
+	{
+		t = transform;
+		Cursor.lockState = CursorLockMode.Locked;
+		isLocked = true;
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		t.Rotate(Vector3.up * Input.GetAxis("Mouse X") * cameraSpeed.x, Space.World);
+		t.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * cameraSpeed.y, Space.Self);
+
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			isLocked = false;
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (!isLocked)
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+				isLocked = true;
+			}
+		}
+			
+	}
+}
