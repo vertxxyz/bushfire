b182d65d56d7545e199392d32cbe84548dbf1a6e
Thomas Ingram <thomasingram@vertx.xyz>
2018-02-24 19:16:08 +1100

    Potential audio fix and basics for bezier curves

---
+++ Bushfire/Assets/PathGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BezierCurves;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+	public List<BezierCurve3D> pathSegments = new List<BezierCurve3D>();
+
+	public Queue<BezierCurve3D> curves = new Queue<BezierCurve3D>();
+	
+	public void AppendPath()
+	{
+		
+	}
+}
